72fa4300ae1da90e86662a280eb598cdf26d152a
Emperadors7 <kayaka498@gmail.com>
2026-05-10 18:20:43 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Bomba.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal
+{
+    internal class Bomba
+    {
+    }
+}
---
+++ ProyectoFinal/PrecioCombustible.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal
+{
+    internal class PrecioCombustible
+    {
+        private decimal precioPorLitro;
+        private DateTime fechaActual;
+        public decimal PrecioPorLitro
+        {
+            get { return precioPorLitro; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("El precio por litro debe ser mayor que cero.");
+                }
+                precioPorLitro = value;
+            }
+        }
+        public DateTime FechaActual
+        {
+            get { return fechaActual; }
+        }
+        //Constructor Con precio inicial
+        public PrecioCombustible(decimal precioInicial)
+        {
+            PrecioPorLitro = precioInicial;
+            fechaActual = DateTime.Now;
+        }
+        public PrecioCombustible() { }
+        public decimal CalcularLitros()
+        public decimal CalcularCosto(decimal litros)
+        {
+            if (litros < 0)
+            {
+                throw new ArgumentException("La cantidad de litros no puede ser negativa.");
+            }
+            return PrecioPorLitro * litros;
+        }
+    }   
+}
